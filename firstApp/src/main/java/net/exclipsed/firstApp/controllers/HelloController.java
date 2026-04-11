fbc2bd31cf70894d26f1773da1f065bd11615350
MicahJJacobson <micahjonjacobson@gmail.com>
2026-04-11 15:14:13 -0700

    Hello World Created
    Added the Hello Controller to output Hello World when the URL is accessed.

---
+++ firstApp/src/main/java/net/exclipsed/firstApp/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package net.exclipsed.firstApp.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Hello World";
+    }
+
+
+}
---
+++ firstApp/src/main/java/net/exclipsed/firstApp/controllers/OrderController.java
@@ -0,0 +1,14 @@
+package net.exclipsed.firstApp.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class OrderController {
+    
+    @GetMapping("/order")
+    public String order()
+    {
+
+    }
+
+}
